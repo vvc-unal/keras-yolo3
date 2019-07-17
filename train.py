b6d532fad88693d361ecef7c17100caed9c78368
Juan Navarro <jsnavarroa@unal.edu.co>
2019-07-16 23:06:27 -0500

    Plot training log

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 """
 Retrain the YOLO model for your own dataset.
 """
+import csv
+from datetime import datetime
 from pathlib import Path
 
 import numpy as np
@@ -8,7 +10,7 @@ import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, CSVLogger
 import matplotlib.pyplot as plt
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
@@ -39,7 +41,7 @@ def yolov3_training():
              unfreeze_epochs=50)
     
 def tiny_yolov3_training():
-    model_name = 'tiny-yolov3-transfer'
+    model_name = 'tiny-yolov3'
     classes_path = 'model_data/coco_classes.txt'
     anchors_path = 'model_data/tiny_yolo_anchors.txt'
     
@@ -49,15 +51,15 @@ def tiny_yolov3_training():
         
     assert len(anchors)==6 # default setting
     
-    model = create_tiny_model(input_shape, anchors, num_classes,
+    model = create_tiny_model(input_shape, anchors, num_classes, load_pretrained=False,
             freeze_body=2, weights_path='model_data/yolov3-tiny_weights.h5')
     
     training(model_name=model_name, 
              model=model, 
              classes_path=classes_path, 
              anchors_path=anchors_path, 
-             frozen_epochs=1, 
-             unfreeze_epochs=0)
+             frozen_epochs=0, 
+             unfreeze_epochs=2)
 
 def vvc_yolov3_training():
     model_name = 'vvc2-yolov3'
@@ -69,33 +71,60 @@ def vvc_yolov3_training():
     anchors = get_anchors(anchors_path)
     
     assert len(anchors)==6 # default setting
-        
-    model = create_vvc_model(input_shape, anchors, num_classes)
+            
+    model = create_vvc_model(yolo3_model.vvc2_yolo_body, input_shape, anchors, num_classes)
     
-    training(model_name=model_name, 
+    training(model_name=model_name,
              model=model, 
              classes_path=classes_path, 
              anchors_path=anchors_path, 
-             unfreeze_epochs=50)
+             unfreeze_epochs=5)
     
+        
+def read_training_log(model_name):
+    model_folder = Path('model_data/' + model_name)
+    log_file = model_folder.joinpath('training_log.csv')
+    
+    training_log = {
+            'model_name' : model_name,
+            'epoch': [],
+            'loss': [],
+            'val_loss': [],
+        }
+    
+    with open(log_file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)  # skip the headers
+        for row in reader:
+            training_log['epoch'].append(row[0])
+            training_log['loss'].append(row[1])
+            training_log['val_loss'].append(row[2])
+            
+    return training_log
+            
 
 def training(model_name, model, classes_path, anchors_path, frozen_epochs=0, unfreeze_epochs=50):
     
     annotation_path = 'tags/train.txt'
-    log_dir = 'logs/' + model_name + '/'
-    log_dir = Path('logs/').joinpath(model_name + '/')
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    log_dir = Path('logs/'+ model_name + ' f{} u{} {}/'.format(frozen_epochs, unfreeze_epochs,timestamp))
     log_dir.mkdir(exist_ok=True)
+    model_folder = Path('model_data/' + model_name)
+    model_folder.mkdir(exist_ok=True)
    
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
     
+    # Callbacks
     logging = TensorBoard(log_dir=log_dir)
-    checkpoint = ModelCheckpoint(str(log_dir) + '/' + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+    checkpoint = ModelCheckpoint(str(log_dir.joinpath('ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5')),
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=5)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+    csv_logger = CSVLogger(str(model_folder.joinpath('training_log.csv')))
 
+    # Train/Val split
     val_split = 0.2
     with open(annotation_path) as f:
         lines = f.readlines()
@@ -114,13 +143,13 @@ def training(model_name, model, classes_path, anchors_path, frozen_epochs=0, unf
 
         batch_size = 32
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        history = model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
                 epochs=frozen_epochs,
                 initial_epoch=0,
-                callbacks=[logging, checkpoint])
+                callbacks=[logging, checkpoint, csv_logger])
         model.save_weights(log_dir.joinpath('trained_weights_stage_1.h5'))
 
     # Unfreeze and continue training, to fine-tune.
@@ -139,38 +168,63 @@ def training(model_name, model, classes_path, anchors_path, frozen_epochs=0, unf
             validation_steps=max(1, num_val//batch_size),
             epochs=frozen_epochs + unfreeze_epochs,
             initial_epoch=frozen_epochs,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+            callbacks=[logging, checkpoint, csv_logger, reduce_lr, early_stopping])
+        
         model.save_weights(log_dir.joinpath('trained_weights_final.h5'))
     
-    model_folder = Path('model_data/' + model_name)
-    model_folder.mkdir(exist_ok=True)
+   
     model.save_weights(model_folder.joinpath('weights.h5'))
 
     # Further training if needed.
     
-    plot_training_history(history)
+    plot_training_history(history, model_folder.joinpath('history.png'))
+        
     
-    
-def plot_training_history(history):
+def plot_training_history(history, save_path):
     # Plot training & validation loss values
    
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     
-    ax1.plot(history.history['loss'])
-    ax1.plot(history.history['val_loss'])
-    ax1.set_ylabel('Loss')
+    ax1.plot(history.epoch, history.history['loss'], label='Train')
+    ax1.plot(history.epoch, history.history['val_loss'], label='Val')
+    
     ax1.set_xlabel('Epoch')
-    ax1.legend(['Train', 'Val'], loc='lower right')
+    ax1.set_ylabel('Loss')
+    ax1.legend(loc='lower left')
     
     ax2.plot(history.history['lr'], 'k--')
     ax2.set_ylabel('Learning rate')
-    ax2.legend(['learning rate'], loc='upper left')
+    ax2.legend(['learning rate'], loc='upper right')
     
     plt.title('Model loss')
+    plt.tight_layout()
+
+    plt.savefig(save_path)
     
     plt.show()
 
+def plot_training_log():
+    # Plot training & validation loss values
+   
+    log = read_training_log('tiny-yolov3')
+    log2 = read_training_log('vvc2-yolov3')
+    
+    fig, ax1 = plt.subplots()
+    
+    ax1.plot(log['epoch'], log['loss'], label=(log['model_name']+'_Train'))
+    ax1.plot(log['epoch'], log['val_loss'], label=(log['model_name']+'_Val'))
+    ax1.plot(log2['epoch'], log2['loss'], label=(log2['model_name']+'_Train'))
+    ax1.plot(log2['epoch'], log2['val_loss'], label=(log2['model_name']+'_Val'))
+    
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss')
+    ax1.legend(loc='lower left')
+    
+    plt.title('Model loss')
+    plt.tight_layout()
+    
+    plt.show()
 
 
 def get_classes(classes_path):
@@ -248,7 +302,7 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
 
     return model
 
-def create_vvc_model(input_shape, anchors, num_classes):
+def create_vvc_model(body_func, input_shape, anchors, num_classes):
     '''create the training model, for VVC YOLOv3'''
     K.clear_session() # get a new session
     image_input = Input(shape=(None, None, 3))
@@ -258,7 +312,7 @@ def create_vvc_model(input_shape, anchors, num_classes):
     y_true = [Input(shape=(h//{0:32, 1:16}[l], w//{0:32, 1:16}[l], \
         num_anchors//2, num_classes+5)) for l in range(2)]
 
-    model_body = yolo3_model.vvc2_yolo_body(image_input, num_anchors//2, num_classes)
+    model_body = body_func(image_input, num_anchors//2, num_classes)
     print('Create VVC YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
 
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
@@ -294,3 +348,4 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
 
 if __name__ == '__main__':
     vvc_yolov3_training()
+    plot_training_log()
